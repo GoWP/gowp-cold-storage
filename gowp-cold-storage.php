2addf8cb6eb93cb0d6f3a759f4390b1ec3707493
Lucas Karpiuk <lucas@gowp.com>
2018-11-17 14:42:55 -0500

    ready for wp.org submission

--- gowp-cold-storage.php
+++ gowp-cold-storage.php
@@ -1,60 +1,107 @@
 <?php
+
 /**
- * Cold Storage
+ * GoWP Cold Storage
  *
  * @wordpress-plugin
  * Plugin Name: GoWP Cold Storage
- * Description: Prevents access to all files/assets of inactive plugins
+ * Description: Prevents access to assets of inactive plugins/themes via .htaccess rules
+ * Version:     1.0.0
  * Author:      GoWP
  * Author URI:  https://www.gowp.com
  * Text Domain: gowp-cold-storage
- * Version:     1.0.0
- * Requires:    1.5.0
- * Tested:      4.9.8
- * Update URI:  https://tools.gowp.com/plugins
  */
 
-/* Update Checker */
+if ( ! defined( 'WPINC' ) ) die;
 
-	require 'plugin-update-checker/plugin-update-checker.php';
-	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://tools.gowp.com/plugins/gowp-cold-storage/info.json',
-		__FILE__
+register_activation_hook( __FILE__, 'gowp_cold_storage_update_theme_rules' );
+register_deactivation_hook( __FILE__, 'gowp_cold_storage_remove_rules' );
+register_uninstall_hook( __FILE__, 'gowp_cold_storage_uninstall' );
+add_action( 'update_option_active_plugins', 'gowp_cold_storage_update_plugin_rules', 10, 3 );
+add_action( 'update_option_stylesheet', 'gowp_cold_storage_update_theme_rules', 10, 3 );
+add_action( 'admin_notices', 'gowp_cold_storage_admin_notice' );
+
+function gowp_cold_storage_uninstall() {
+	delete_option( 'gowp_cold_storage_update_failures' );
+}	
+
+function gowp_cold_storage_update_plugin_rules( $old, $new, $option ) {
+	$file = str_replace( WP_PLUGIN_DIR, '', implode( "/", array_slice( explode( "/", str_replace( "trunk/", "", __FILE__ ) ), -2, 2 ) ) );
+	if ( in_array( $file, $new ) ) {
+		$rules = gowp_cold_storage_get_plugin_rules( $new );
+		gowp_cold_storage_write_rules( $rules, 'plugins' );
+	}
+}
+
+function gowp_cold_storage_get_plugin_rules( $active ) {
+	if ( ! function_exists( 'get_plugins' ) ) {
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+	}
+	$plugins = get_plugins();
+	foreach ( $plugins as $plugin => $data ) {
+		if ( ! in_array( $plugin, $active ) ) {
+			$slugs[] = dirname( $plugin );
+		}
+	}
+	if ( ! empty( $slugs ) ) {
+		$rules[] = 'RewriteRule ^(' . implode( '|', $slugs ) . ')/.*$ - [F]';
+		return $rules;
+	}
+}
+
+function gowp_cold_storage_update_theme_rules( $old = NULL, $new = NULL, $option = NULL ) {
+	$rules = gowp_cold_storage_get_theme_rules( $new );
+	gowp_cold_storage_write_rules( $rules, 'themes' );
+}
+
+function gowp_cold_storage_get_theme_rules( $active ) {
+	$themes = wp_get_themes();
+	if ( empty( $active ) ) $active = get_stylesheet();
+	foreach ( $themes as $slug => $theme ) {
+		if ( ( $slug != $active ) && ( $slug != $themes[ $active ]->Template ) ) {
+			$slugs[] = $slug;
+		}
+	}
+	if ( ! empty( $slugs ) ) {
+		$rules[] = 'RewriteRule ^(' . implode( '|', $slugs ) . ')/.*$ - [F]';
+		return $rules;
+	}
+}
+
+function gowp_cold_storage_write_rules( $rules, $context ) {
+	if ( ! empty( $rules ) ) {
+		array_unshift(
+			$rules,
+			'RewriteEngine On',
+			'RewriteCond %{HTTP_REFERER} !customize_changeset_uuid [NC]'
+		);
+	}
+	$files = array(
+		'plugins' => WP_PLUGIN_DIR . '/.htaccess',
+		'themes' => get_theme_root() . '/.htaccess',
 	);
-
-/* Update .htaccess rules when one or more plugin's status changes */
-
-	add_action( 'update_option_active_plugins', 'gcs_update_rules', 10, 3 );
-	function gcs_update_rules( $old, $new, $option ) {
-		$file = str_replace( WP_PLUGIN_DIR, '', implode( "/", array_slice( explode( "/", __FILE__ ), -2, 2 ) ) ); // eg. folder/plugin.php
-		if ( ! in_array( $file, $new ) ) { // this plugin has been deactivated, remove all rules
-			$rules = "";
-		} else {
-			if ( ! function_exists( 'get_plugins' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
-			}
-			$plugins = get_plugins();
-			foreach ( $plugins as $plugin => $data ) { // all installed plugins
-				if ( ! in_array( $plugin, $new ) ) { // inactive plugin
-					$folder = str_replace( home_url( '/' ), '', plugins_url() . "/" . dirname( $plugin ) ); // eg. /wp-content/plugins/folder/plugin.php
-					$rules[] = "RedirectMatch 403 ^/{$folder}/.*$";
-				}
-			}
-		}
-		if ( ! insert_with_markers( WP_PLUGIN_DIR . "/.htaccess", "GoWP Cold Storage", $rules ) ) {
-			add_option( 'gsc_update_failed', $rules );
-		} else {
-			delete_option( 'gsc_update_failed' );
-		}
-		
+	$file = $files[ $context ];
+	$gowp_cold_storage_update_failures = get_option( 'gowp_cold_storage_update_failures' );
+	if ( insert_with_markers( $file, "GoWP Cold Storage", $rules ) ) {
+		unset( $gowp_cold_storage_update_failures[ $file ] );
+	} else {
+		$gowp_cold_storage_update_failures[ $file ] = $rules;
 	}
+	update_option( 'gowp_cold_storage_update_failures', $gowp_cold_storage_update_failures, FALSE );
+}
 
-/* Admin notice */
+function gowp_cold_storage_remove_rules() {
+	gowp_cold_storage_write_rules( "", 'plugins' );
+	gowp_cold_storage_write_rules( "", 'themes' );
+}
 
-	add_action( 'admin_notices', 'gsc_admin_notice' );
-	function gsc_admin_notice() {
-		if ( $rules = get_option( 'gsc_update_failed' ) ) {
-			$notice = translate( 'GoWP Cold Storage was unable to update configuration. Please ensure that <code>' . WP_PLUGIN_DIR . '/.htaccess</code> is writeable.' );
-			echo "<div class='notice notice-error is-dismissable'><p>$notice</p></div>";
+function gowp_cold_storage_admin_notice() {
+	if ( $gowp_cold_storage_update_failures = get_option( 'gowp_cold_storage_update_failures' ) ) {
+		echo "<div class='notice notice-error is-dismissable'>";
+		echo "<p>GoWP Cold Storage was unable to update configuration.</p>";
+		foreach ( $gowp_cold_storage_update_failures as $file => $rules ) {
+			echo "<p>Please ensure that <code>{$file}</code> is writeable.</p>";
 		}
+		echo "</div>";
 	}
+}
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== GoWP Cold Storage ===
+Contributors: karpstrucking, another-guy
+Tags: htaccess, themes, plugins, security, assets
+Requires at least: 3.4
+Tested up to: 4.9.8
+Requires PHP: 5.6
+Stable tag: trunk
+
+Prevents access to assets of inactive plugins/themes via .htaccess rules
+
+== Description ==
+
+This plugin prevents access to assets of inactive plugins and themes. Rules are enforced via `.htaccess` files, updated each time the active status of a plugin or theme changes.
+
+[GitHub](https://github.com/GoWP/gowp-cold-storage)
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
